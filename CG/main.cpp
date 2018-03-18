37fe2aeb21a8bdbc86e5c9e30bf9e0bb6457b636
Jessica <jessica121353@gmail.com>
2018-03-18 23:04:56 +0800

    Initial Commit

---
+++ CG/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  CG
+//
+//  Created by Jessica on 3/18/18.
+//  Copyright © 2018 Jessica. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
